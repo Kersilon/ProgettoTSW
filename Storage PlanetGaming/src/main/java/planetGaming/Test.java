9d33faf5a76e095bc6cd3cd27505810584c8c0b4
Kersilon <ersiliopet@gmail.com>
2022-06-19 11:33:06 +0200

    Spostato la classe test

---
+++ Storage PlanetGaming/src/main/java/planetGaming/Test.java
@@ -0,0 +1,22 @@
+package planetGaming;
+import java.sql.SQLException;
+
+import planetGaming.Indirizzo.IndirizzoBean;
+import planetGaming.Indirizzo.IndirizzoDAO;
+
+public class Test {
+	IndirizzoBean indBean;
+	IndirizzoDAO indDao;
+	
+	
+	public void test(){
+		indBean = null;
+		
+		try {
+			indDao.doSave(indBean);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
